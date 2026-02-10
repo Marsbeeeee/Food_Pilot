98c1afd5db146aa4c60043c5a9a3faebbd26a273
XiaoYi <huxiaoyi05922@gmail.com>
2026-02-10 14:34:11 +0800

    allow frontend call backend port

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from backend.routers.health import router as health_router
+from fast.api.middleare.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:3000", "http://100.64.164.2:3000"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 
 app.include_router(health_router)
